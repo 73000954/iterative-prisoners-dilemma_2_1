af96ef8f8891cee5bf75e4d36eadcb337d0afa85
Jarod Hansen <jarod.hansen@students.sacs.k12.in.us>
2015-12-16 09:58:15 -0500

    Update prisoners_dilemma.py
    I added a player number 25!

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -581,6 +581,26 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
                 return 'c' #otherwise collude
     
     
+    #HHHHHHHHHHHHHHHHHIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+
+
+    ######
+    ######
+    #
+    elif player == 25:
+        if getting_team_name:
+            return 'loyal vengeful'
+        else:
+            # use history, opponent_history, score, opponent_score
+            # to compute your strategy
+            if len(opponent_history)==0: #It's the first round: collude
+                return 'c'
+        if history[-1]=='c' and opponent_history[-1]=='b':
+            if opponent_history[-1]== 'c':    
+                return 'b' # betray is they were severely punished last time
+            else:
+                return 'c' #otherwise colude 
+    
 
 
 
